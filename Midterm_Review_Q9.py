00582f2008f9d3fb1f2a232746893917373e34e6
abhaysamantni <61202612+abhaysamantni@users.noreply.github.com>
2023-03-06 16:54:43 -0600

    Update Midterm_Review_Q9.py

--- Midterm_Review_Q9.py
+++ Midterm_Review_Q9.py
@@ -1,3 +1,5 @@
+#Practice for linked list. No specific question, just a practice and review code
+
 # A linked list is a sequence of data elements, which are connected together via links. Each data element contains a
 # connection to
 # another data element in form of a pointer. Python does not have linked lists in its standard library.
