700ca1789e320dd67967563f7860293a98127b11
abhaysamantni <abhay.samant@ni.com>
2023-03-05 20:38:03 -0600

    modified q4 to fix issues with max and min on string

--- Midterm_Review_Q4.py
+++ Midterm_Review_Q4.py
@@ -13,7 +13,7 @@
 #
 # Hottest day in the year was 8/12/2016
 # Coldest day in the year was 12/19/2016
-import matplotlib.pyplot as plt
+
 f1=open('datefile.txt','r')
 f2=open('maxTempfile.txt','r')
 f3=open('minTempfile.txt', 'r')
@@ -22,13 +22,11 @@ data2d=[[]]
 #x_coords=[]
 #y_coords=[]
 data2d = [[0 for i in range(366)] for i in range(4)]
-#x_coords = [0 for i in range(366)]
-#y_coords = [0 for i in range(366)]
-#print(data2d)
+
 date=f1.readline().rstrip('\n')
 maxt=f2.readline().rstrip('\n')
 mint=f3.readline().rstrip('\n')
-avgt=f3.readline().rstrip('\n')
+avgt=f4.readline().rstrip('\n')
 i=0
 while(date!=''):
     data2d[0][i]=date
@@ -40,20 +38,15 @@ while(date!=''):
     mint=f3.readline().rstrip('\n')
     avgt=f4.readline().rstrip('\n')
     i+=1
-#Find the hottest day of the year
-max_index=(data2d[1][0:366]).index(max(data2d[1][0:366]))
+#Convert string list to integer list using map function before finding the hottest day of the year
+temp_int_list=list(map(int, data2d[1][0:366]))
+max_index=(temp_int_list).index(max(temp_int_list))
 print("Hottest day in the year was", (data2d[0][0:366])[max_index])
-#Find the coldest day of the year
-#print(data2d[2][0:366])
-#coldesttemp=list(map(int, (data2d[2][0:366])))[0]
-#print("Coldest day temp", coldesttemp)
-min_index=(data2d[2][0:366]).index(min(data2d[2][0:366]))
+#Convert string list to integer list before finding the coldest day of the year
+temp_int_list=list(map(int, data2d[2][0:366]))
+min_index=(temp_int_list).index(min(temp_int_list))
 print("Coldest day in the year was",(data2d[0][0:366])[min_index])
 
-
-plt.yticks([0, 10, 20, 30, 40, 50],['0', '20', '40', '60', '80', '100'])
-plt.plot(data2d[3][0:366])
-plt.show()
 f1.close()
 f2.close()
 f3.close()
